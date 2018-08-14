4d962d0ceca6394563e6dbb4c80ee237d6cc554d
Carolina Chamorro <carolinachasa@gmail.com>
2018-08-14 14:02:36 +0200

    refactor ordenar alfabeticamente

--- index.js
+++ index.js
@@ -1,3 +1,115 @@
+/**
+  *  MOCK_DATA.json
+  */
+let rawFile = new XMLHttpRequest();
+rawFile.open("GET", 'MOCK_DATA.json', true);
+rawFile.send(null);
+rawFile.onload = function() {
+    var tripulationMembers = rawFile.response;
+    if (rawFile.readyState === 4 && rawFile.status == "200") {
+        show(tripulationMembers);
+    } else {
+        alert('ERROR!!');
+    }
+  }
+
+function show() {
+    let tripulationBox = document.querySelector('#tripulation');
+    let a = eval(rawFile.responseText);
+
+    const human ='human';
+    const vulcano = 'vulcano';
+    const betazoid = 'betazoid';
+    const command= 'command';
+    const science = 'science';
+    const engineering = 'engineering';
+    
+    let myArticle = document.createElement('div');
+    for (let i = 0; i < a.length; i++) {
+        let position = a[i].position;
+        let race = a[i].race;
+        let name = a[i].name;
+        let gender = a[i].gender;
+        let ip = a[i].ip_address;
+            
+                b = '<center id="center"><div id="box">'
+                if (a[i].position == science) {
+                    b += '<div class="boxColour blue"></div>' 
+
+                } else if (a[i].position == engineering) {
+                    b += '<div class="boxColour red"></div>' 
+
+                } else {
+                    b += '<div class="boxColour yellow"></div>' 
+                }
+
+                b += '<div class="dates"><div id="box_name">' + name + '</div>'
+                b += '<div id="box_gender">' + 'Gender: ' + '<div class="styleText">' + gender + '</div></div>'
+                b += '<div id="box_id">' + 'Id: ' + ip + '</div>'
+                b += '<div id="box_race">' + 'Race: ' + race + '</div>'
+                b += '<div id="box_position">' + 'Position: ' + position + '</div></div>'
+
+                if (race == human) {
+                    switch (position) {
+                        case command:
+                            b += '<div class="boxNumber">x3</div>'
+                            break;
+                        case science:
+                            b += '<div class="boxNumber">x1</div>'
+                            break;
+                        case engineering:
+                            if (name == 'Scotty') {
+                                b += '<div class="boxNumber">x4</div>'
+                            break;
+                            } else {
+                                b += '<div class="boxNumber">x2</div>'
+                            break;
+                            }
+                        default:
+                        console.log("ERROR race == human");
+                    }
+
+                } else if(race == vulcano) {
+                    switch (position) {
+                        case command:
+                            b += '<div class="boxNumber">x2</div>'
+                            break;
+                        case engineering:
+                            b += '<div class="boxNumber">x1</div>'
+                            break;
+                        case science:
+                            b += '<div class="boxNumber">x3</div>'
+                            break;
+                        default:
+                        console.log("ERROR race == vulcano");
+                        }
+
+                } else if (race == betazoid){
+                    switch (position) {
+                        case command:
+                            b += '<div class="boxNumber">x1</div>'
+                            break;
+                        case engineering:
+                            b += '<div class="boxNumber">x3</div>'
+                            break;
+                        case science:
+                            b += '<div class="boxNumber">x2</div>'
+                            break;
+                        default:
+                        console.log("race == betazoid");
+                    }
+
+                }
+                b +=  '</div></center>'
+            
+
+        document.body.innerHTML += b;
+    }
+    tripulationBox.appendChild(myArticle);
+}
+
+
+
 //onclick='orderPosition()'
 function orderPosition() {
     let center = document.getElementsByTagName('center');
@@ -108,139 +220,33 @@ function orderPosition() {
         document.querySelector('#tripulation').append(newArray[i]);
     }
 }
-//onclick='orderAlphabet()'
+
+
+// onclick='orderAlphabet()'
 function orderAlphabet() {
     c = document.getElementsByTagName('center');
     newArray = [];
-    for (var i = 0; i < c.length; i++) {
+    for (let i = 0; i < c.length; i++) {
         newArray.push(c[i]);
     }
-    newArray.shift();
-    for (var i = 0; i < d.length; i++) {
-        for(var j=0; j < d.length - 1; j++){
+    newArray.sort(function() {
+        for(let j=0; j < newArray.length - 1; j++){
             // obtengo las dos razas
             n1 = newArray[j].querySelector('div').innerText;
-            n2 = newArray[j+1].querySelector('div').innerText;            
+            n2 = newArray[j + 1].querySelector('div').innerText;
+
             if (n1 > n2) {
-                var z = newArray[j]
-                var y = newArray[j + 1]
+                let z = newArray[j]
+                let y = newArray[j + 1]
                 newArray[j] = y
                 newArray[j + 1] = z
             }
         }
-    }
+
+    });
+    
     document.querySelector('#tripulation').innerHTML = '';
     for (i = 0; i < newArray.length; i++) {
         document.querySelector('#tripulation').append(newArray[i]);
     }
-}
-
-/**
-  *  MOCK_DATA.json
-  */
-let rawFile = new XMLHttpRequest();
-rawFile.open("GET", 'MOCK_DATA.json', true);
-rawFile.send(null);
-rawFile.onload = function() {
-    var tripulationMembers = rawFile.response;
-    if (rawFile.readyState === 4 && rawFile.status == "200") {
-        show(tripulationMembers);
-    } else {
-        alert('ERROR!!');
-    }
-  }
-
-function show() {
-    let tripulationBox = document.querySelector('#tripulation');
-    a = eval(rawFile.responseText);
-
-    const human ='human';
-    const vulcano = 'vulcano';
-    const betazoid = 'betazoid';
-    const command= 'command';
-    const science = 'science';
-    const engineering = 'engineering';
-    
-    let myArticle = document.createElement('div');
-    for (let i = 0; i < a.length; i++) {
-        let position = a[i].position;
-        let race = a[i].race;
-        let name = a[i].name;
-        let gender = a[i].gender;
-        let ip = a[i].ip_address;
-            
-                b = '<center id="center"><div id="box">'
-                if (a[i].position == science) {
-                    b += '<div class="boxColour blue"></div>' 
-
-                } else if (a[i].position == engineering) {
-                    b += '<div class="boxColour red"></div>' 
-
-                } else {
-                    b += '<div class="boxColour yellow"></div>' 
-                }
-
-                b += '<div class="dates"><div id="box_name">' + name + '</div>'
-                b += '<div id="box_gender">' + 'Gender: ' + '<div class="styleText">' + gender + '</div></div>'
-                b += '<div id="box_id">' + 'Id: ' + ip + '</div>'
-                b += '<div id="box_race">' + 'Race: ' + race + '</div>'
-                b += '<div id="box_position">' + 'Position: ' + position + '</div></div>'
-
-                if (race == human) {
-                    switch (position) {
-                        case command:
-                            b += '<div class="boxNumber">x3</div>'
-                            break;
-                        case science:
-                            b += '<div class="boxNumber">x1</div>'
-                            break;
-                        case engineering:
-                            if (name == 'Scotty') {
-                                b += '<div class="boxNumber">x4</div>'
-                            break;
-                            } else {
-                                b += '<div class="boxNumber">x2</div>'
-                            break;
-                            }
-                        default:
-                        console.log("ERROR race == human");
-                    }
-
-                } else if(race == vulcano) {
-                    switch (position) {
-                        case command:
-                            b += '<div class="boxNumber">x2</div>'
-                            break;
-                        case engineering:
-                            b += '<div class="boxNumber">x1</div>'
-                            break;
-                        case science:
-                            b += '<div class="boxNumber">x3</div>'
-                            break;
-                        default:
-                        console.log("ERROR race == vulcano");
-                        }
-
-                } else if (race == betazoid){
-                    switch (position) {
-                        case command:
-                            b += '<div class="boxNumber">x1</div>'
-                            break;
-                        case engineering:
-                            b += '<div class="boxNumber">x3</div>'
-                            break;
-                        case science:
-                            b += '<div class="boxNumber">x2</div>'
-                            break;
-                        default:
-                        console.log("race == betazoid");
-                    }
-
-                }
-                b +=  '</div></center>'
-            
-
-        document.body.innerHTML += b;
-    }
-    tripulationBox.appendChild(myArticle);
 }
